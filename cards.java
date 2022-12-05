da5e34263354221832b7af1ec3b3352fe8f4cc00
hsnefe <119686500+hsnefe@users.noreply.github.com>
2022-12-05 14:09:08 +0300

    forgot to save the "cards" class

---
+++ cards.java
@@ -0,0 +1,13 @@
+package pisti;
+
+public class cards {
+    public String color;
+    public String suit;
+    public int number;
+    public cards(String a,String b,int c){
+        color = a;
+        suit = b;
+        number = c;
+    }
+    
+}

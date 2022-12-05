2e55e8825b9709df22963fa0071b1fbaaf0f6f8a
hsnefe <119686500+hsnefe@users.noreply.github.com>
2022-12-05 14:06:24 +0300

    preparing cards-part1
    Suits are ready

--- pisti.java
+++ pisti.java
@@ -1,8 +1,37 @@
 package pisti;
 
 public class pisti {
-    public static void main(String[] args){
-        
-    }
+
     
-}
+    public  static cards[] suiter(int a,int b,cards[] tar){
+        switch(a){
+            case(0):
+            tar[a].suit = "♠";
+            break;
+            case(1):
+            tar[a].suit ="♣";
+            break;
+            case(2):
+            tar[a].suit ="♥";
+            case(3):
+            tar[a].suit ="♦";
+
+            
+        }
+        return tar;
+    }
+    public static void main(String[] args){
+        cards[] card = new cards[51];
+            for(int j = 0;j<4;j++){
+                for(int k = 0;k<12;k++){
+                    suiter(j,k,card);
+                        
+                        
+                        }
+                    }
+                    }      
+                }
+        
+    
+    
+

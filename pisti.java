c806e1de66868107a968703af8c83361ec3dbc5c
hsnefe <119686500+hsnefe@users.noreply.github.com>
2022-12-05 23:01:39 +0300

    added number to the cards
    added number, king, queen and jack to the cards

--- pisti.java
+++ pisti.java
@@ -4,6 +4,23 @@ public class pisti {
 
     
     public  static cards[] suiter(int a,int b,cards[] tar){
+        if(b<9 && b != 0){
+            tar[b].number = String.valueOf(b+1);
+        }
+        switch(b){
+            case(0):
+            tar[b].number = "A";
+            break;
+            case(10):
+            tar[b].number ="JACK";
+            break;
+            case(11):
+            tar[b].number ="QUEEN";
+            break;
+            case(12):
+            tar[b].number ="KING";
+            break;
+        }
         switch(a){
             case(0):
             tar[a].suit = "♠";
@@ -22,7 +39,7 @@ public class pisti {
     }
     public static void main(String[] args){
         cards[] card = new cards[51];
-            for(int j = 0;j<4;j++){
+            for(int j = 0;j<3;j++){
                 for(int k = 0;k<12;k++){
                     suiter(j,k,card);
                         
